df1c80af080218f9b6345d2b2ff1358a31e0cc12
tsuchiya <youtaro1123@gmail.com>
2020-08-10 23:30:28 +0900

    First commit!

---
+++ README.md
@@ -0,0 +1,14 @@
+# bitcoin_report
+ビットコイン価格を定期で slack 通知する
+
+# Setup
+
+```
+$ cp ./app/.env.sample ./app/.env
+```
+
+上記，`./app/.env` へ値を追記
+
+```
+$ docker-compose up
+```
---
+++ app/main.py
@@ -0,0 +1,25 @@
+import requests, json, os
+from bs4 import BeautifulSoup
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from dotenv import load_dotenv
+load_dotenv('.env')
+WEB_HOOK_URL = os.environ.get("WEB_HOOK_URL")
+
+if __name__ == '__main__':
+    url = 'https://api.coin.z.com/public/v1/ticker'
+    params = {'symbol': 'BTC'}
+    response = requests.get(url, params=params)
+    res_items = response.json()
+    bitcoin_price = int(res_items['data'][0]['last'])
+
+    message_json = json.dumps({
+        'text'       : '現在の BITCOIN 価格は {bitcoin_price}円 です'.format(bitcoin_price=bitcoin_price),
+        'username'   : 'BITCOIN REPORT',
+        'icon_emoji' : ':bitcoin:',
+        'link_names' : 1
+    })
+
+    requests.post(WEB_HOOK_URL, data=message_json)
